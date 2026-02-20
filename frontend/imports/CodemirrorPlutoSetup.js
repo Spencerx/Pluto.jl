9dc6b97d4360ee6817564c3227ccf630952eb20f
Fons van der Plas <fonsvdplas@gmail.com>
2026-02-20 17:24:53 +0100

    Update CodemirrorPlutoSetup.js

--- frontend/imports/CodemirrorPlutoSetup.js
+++ frontend/imports/CodemirrorPlutoSetup.js
@@ -1,2 +1,2 @@
 // @ts-ignore
-export * from "https://cdn.jsdelivr.net/gh/JuliaPluto/codemirror-pluto-setup@2002.0.3/dist/index.es.min.js"
+export * from "https://cdn.jsdelivr.net/gh/JuliaPluto/codemirror-pluto-setup@2002.0.4/dist/index.es.min.js"
